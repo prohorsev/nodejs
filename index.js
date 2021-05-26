dcbd397e1d3928f70b05169f7c378b1a31eebb28
Prohor Sevastianov <sevastyanov.p@oneway.su>
2021-05-26 22:43:23 +0300

    разделил на функции и убрал метку

--- index.js
+++ index.js
@@ -6,38 +6,54 @@ max - верхний предел диапазона чисел
 */
 const colors = require("colors/safe");
 
-let min = Number(process.argv[2]);
-let max = Number(process.argv[3]);
-getPrimeNumbers(min, max);
+function run() {
+    const min = Number(process.argv[2]);
+    const max = Number(process.argv[3]);
+    if (validateArguments(min, max)) {
+        showResult(findPrimeNumbers(min, max));
+    }
+}
 
-function getPrimeNumbers(min, max) {
+function validateArguments(min, max) {
     if (!Number.isInteger(min) || !Number.isInteger(max)) {
         console.log(colors.red("Аргумент не является целым числом"));
-        return;
+        return false;
     }
     if (min < 2 || max < 2) {
         console.log(colors.red("Аргумент должен быть больше 1"));
-        return;
+        return false;
     }
     if (min > max) {
         console.log(colors.red("Первый аргумент должен быть меньше второго"));
-        return;
+        return false;
     }
+    return true;
+}
+
+function findPrimeNumbers(min, max) {
     let numbers = [];
-    nextPrime:
-    for (let i = min; i <= max; i++) {
-        for (let j = 2; j < i; j++) {
-            if (i % j === 0) {
-                continue nextPrime;
+        for (let i = min; i <= max; i++) {
+            if (isPrime(i)) {
+                numbers.push(i);
             }
         }
-       numbers.push(i);
-    }
+    return numbers;
+}
+
+function isPrime(num) {
+    for(var i = 2; i < num; i++)
+        if(num % i === 0) return false;
+    return num > 1;
+}
+
+function showResult(numbers) {
     if (numbers.length < 1) {
         console.log(colors.red("Нет простых чисел"));
-        return;
+        return false;
     }
     console.log(colors.green(numbers[0]));
     if (numbers[1]) console.log(colors.yellow(numbers[1]));
     if (numbers[2]) console.log(colors.red(numbers[2]));
 }
+
+run();
